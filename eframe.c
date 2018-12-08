5fcc31a21fa2919027c68494b4116252b0415979
yulincoder <784773435@qq.com>
2018-12-08 14:59:47 +0800

    add license

--- eframe.c
+++ eframe.c
@@ -1,5 +1,5 @@
 /*
- * Copyright (c) 2018 Google LLC
+ * Copyright (c) 2018 SenseAge LLC. ZhangT
  *
  * Permission is hereby granted, free of charge, to any person obtaining a copy of
  * this software and associated documentation files (the "Software"), to deal in
